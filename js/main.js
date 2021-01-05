98c9125c7894b974721f006ec131e84ac9d09835
Tomasz Belech <tomaszbelech1@gmail.com>
2021-01-05 12:21:46 +0100

    TO the top button added

--- js/main.js
+++ js/main.js
@@ -31,3 +31,23 @@ function initMap() {
     map: map,
   });
 }
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
